419cc97e7211440f464b9acc9ef7cbeea55e2e9a
ABDUL-REVEALERS <abcode@yahoo.com>
2025-05-30 10:51:52 +0100

    Describe what you changedfdssds

--- components/footer.tsx
+++ components/footer.tsx
@@ -7,7 +7,7 @@ const Footer = () => {
     const currentYear = new Date().getFullYear();
     return <footer className="border-t">
         <div className="p-5 flex-center">
-            {currentYear} {APP_NAME}. All Rights Reserved
+          &copy;  {currentYear} {APP_NAME}. All Rights Reserved
         </div>
     </footer>;
 }
