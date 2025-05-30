5c086e2d2a2a870e25dc34adc8b71c4a44338c4e
ABDUL-REVEALERS <abcode@yahoo.com>
2025-05-30 09:53:05 +0100

    Describe what you changedfdssds

--- app/student/courses/details/[slug]/page.tsx
+++ app/student/courses/details/[slug]/page.tsx
@@ -140,7 +140,7 @@ export default function StudentCourseDetailsPage() {
   };
 
   return (
-    <div className="max-w-4xl mx-auto p-4">
+    <div className="max-w-full mx-auto p-4">
       {course && (
         <>
           <h1 className="text-2xl font-bold mb-2">{course.title}</h1>
